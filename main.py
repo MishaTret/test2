b1826e8b5f9c71b00dd68de75bd253b0ddf583bc
macbook <mishatret2010@gmail.com>
2023-01-21 14:55:44 +0200

    test 1

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("main")
+class Car:
+    wheels = 4
+    speed = 120
+    color = 'Green'
+    power = 85
+
+
+print(Car.wheels)
+
+class Table:
+    height = 90
+    width = 120
+
+
+print(Table.height)
